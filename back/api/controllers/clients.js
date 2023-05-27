18ceb0aa62b253317ac3287daa2650ab8960a144
Luis Pedro Gonzalez Aldana <60456157+LPELCRACK896@users.noreply.github.com>
2023-05-27 17:00:32 -0600

    get clients route

--- back/api/controllers/clients.js
+++ back/api/controllers/clients.js
@@ -1,10 +1,17 @@
 const ErrorResponse = require('../utils/errorResponse')
 const asyncHandler = require('../middlewares/async')
 
-// @desc    Get all bootcamps
-// @route   GET /api/v1/bootcamps
+// @desc    Get all clients
+// @route   GET /api/v1/clients
 // @access   Public
 exports.getClients = asyncHandler(async (req, res, next) => {
-    return res.status(200).json({msg: "First route created"})
+    const driver = req.driver
+    const session = driver.session()
+
+    const result = await session.run('MATCH (n) RETURN n')
+    const nodes = result.records.map(record => record.get('n').properties);
+    session.close()
+
+    return res.status(200).json({msg: "First route created", data: nodes})
 })
 
