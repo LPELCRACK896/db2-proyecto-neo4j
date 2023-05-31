f575784fd22fba1ad6714236ffa77481b620e497
Luis Pedro Gonzalez Aldana <60456157+LPELCRACK896@users.noreply.github.com>
2023-05-31 00:10:17 -0600

    Single transfer fix

--- back/api/controllers/transfers.js
+++ back/api/controllers/transfers.js
@@ -20,14 +20,15 @@ exports.getTransfers = asyncHandler(async (req, res, next) => {
 // @route   GET /api/v1/transfers/:id
 // @access   Public
 exports.getTransfer = asyncHandler(async (req, res, next) => {
+    const id  = req.params.id
     const driver = req.driver
     const session = driver.session()
-    const query = 'MATCH (n) RETURN n' //Cambiar esta linea
+    const query = `MATCH (n: Transfer {id: \"${id}\"}) RETURN n` //Cambiar esta linea
     const result = await session.run(query)
     const nodes = result.records.map(record => record.get('n').properties);
     session.close()
 
-    return res.status(200).json({msg: "Got all transfers", data: nodes})
+    return res.status(200).json({msg: "Got a transfer", data: nodes})
 })
 
 
