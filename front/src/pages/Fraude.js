a06cce5288cae6e6ad62e8d238a971643fcb20e7
KennethGalvez <60400757+KennethGalvez@users.noreply.github.com>
2023-05-31 17:38:54 -0600

    Fraude cositas

--- front/src/pages/Fraude.js
+++ front/src/pages/Fraude.js
@@ -3,18 +3,13 @@ import './Home.css'
 
 const Fraude = () => {
   const [selectedOption, setSelectedOption] = useState('');
-  const [selectedSubOption, setSelectedSubOption] = useState('');
   const [fraudDetected, setFraudDetected] = useState(false);
   const [additionalData, setAdditionalData] = useState('');
 
   const handleOptionChange = (event) => {
     setSelectedOption(event.target.value);
-    setSelectedSubOption('');
   };
 
-  const handleSubOptionChange = (event) => {
-    setSelectedSubOption(event.target.value);
-  };
 
   const handleDetectFraud = () => {
     // Aquí puedes realizar la lógica para detectar el fraude según las opciones seleccionadas
@@ -35,53 +30,13 @@ const Fraude = () => {
             <option value="">Seleccione una opción</option>
             <option value="Account">Transferencia</option>
             <option value="Client">Cliente</option>
-            <option value="Deposit">Cuenta</option>
-            <option value="Person">Cuenta</option>
-            <option value="Transfer">Cuenta</option>
-            <option value="Withdrawal">Cuenta</option>
-            <option value="Todo">Todo</option>
+            <option value="Deposit">Deposit</option>
+            <option value="Person">Person</option>
+            <option value="Transfer">Transfer</option>
+            <option value="Withdrawal">Withdrawal</option>
           </select>
         </label>
       </div>
-
-      {selectedOption && (
-        <div>
-          <label>
-            Subopción:
-            <select value={selectedSubOption} onChange={handleSubOptionChange}>
-              <option value="">Seleccione una subopción</option>
-              {selectedOption === 'transferencia' && (
-                <>
-                  <option value="opcion1">Opción 1</option>
-                  <option value="opcion2">Opción 2</option>
-                  <option value="opcion3">Opción 3</option>
-                </>
-              )}
-              {selectedOption === 'cliente' && (
-                <>
-                  <option value="opcion4">Opción 4</option>
-                  <option value="opcion5">Opción 5</option>
-                  <option value="opcion6">Opción 6</option>
-                </>
-              )}
-              {selectedOption === 'cuenta' && (
-                <>
-                  <option value="opcion7">Opción 7</option>
-                  <option value="opcion8">Opción 8</option>
-                  <option value="opcion9">Opción 9</option>
-                </>
-              )}
-              {selectedOption === 'todo' && (
-                <>
-                  <option value="opcion10">Opción 10</option>
-                  <option value="opcion11">Opción 11</option>
-                  <option value="opcion12">Opción 12</option>
-                </>
-              )}
-            </select>
-          </label>
-        </div>
-      )}
 
       <button onClick={handleDetectFraud}>Detectar Fraude</button>
 
