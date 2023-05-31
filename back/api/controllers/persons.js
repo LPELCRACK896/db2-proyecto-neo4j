0b10a19a5fbcf74601daf063fcdffb0a0ace663e
Luis Pedro Gonzalez Aldana <60456157+LPELCRACK896@users.noreply.github.com>
2023-05-31 17:37:40 -0600

    POST Create Person

--- back/api/controllers/persons.js
+++ back/api/controllers/persons.js
@@ -38,5 +38,30 @@ exports.getPerson = asyncHandler(async (req, res, next) => {
     return res.status(200).json({msg: nodes.length?"Got a person related to that dpi": "Got no data.", data: nodes.length?nodes[0]:nodes})
 })
 
+// @desc    Create one clients
+// @route   POST /api/v1/clients
+// @access   Public
+exports.createClient = asyncHandler(async (req, res, next) => {
+
+    const driver = req.driver
+    const session = driver.session()
+    const { dpi, name } = req.body
+
+    const exist_dpi = await session.run(`MATCH (n: Person {dpi: toInteger($dpi)}) RETURN count(n) > 0 as exists`, { dpi })
+    if (exist_dpi.records[0].get('exists')){
+        return next(new ErrorResponse(`El numero de DPI ya está asociado a una persona en la base de datos.`, 409))
+    }
 
 
+    const createQuery = `CREATE (c:Person {
+        name: $name,
+        dpi: toInteger($dpi)})
+        RETURN c`
+    const result = await session.run(createQuery, {name, dpi})
+    console.log(result)
+    const client = result.records[0].get('c').properties;
+
+    session.close()
+
+    return res.status(201).json({ success: true, data: client });
+})
--- back/api/routes/persons.js
+++ back/api/routes/persons.js
@@ -1,12 +1,14 @@
 const express = require('express')
 const {
     getPersons,
-    getPerson
+    getPerson, 
+    createClient
  } = require('../controllers/persons')
+ const fieldChecker = require('../middlewares/fieldChecker');
 
 const router = express.Router();
 
-router.route('/').get(getPersons)
+router.route('/').get(getPersons).post(fieldChecker(["name", "dpi"]), createClient)
 router.route('/:id').get(getPerson)
 
 module.exports = router
