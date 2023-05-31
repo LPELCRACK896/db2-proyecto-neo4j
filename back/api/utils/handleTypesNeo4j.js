2a930a891a55d2fed2a692197bab673bdb229ed2
Luis Pedro Gonzalez Aldana <60456157+LPELCRACK896@users.noreply.github.com>
2023-05-31 15:41:25 -0600

    Fix handle types neo4j

--- back/api/utils/handleTypesNeo4j.js
+++ back/api/utils/handleTypesNeo4j.js
@@ -1,32 +1,31 @@
 const neo4j = require('neo4j-driver');
 
-
-const toBigInt = (low, high) => {
-    return BigInt(high) << BigInt(32) | BigInt(low);
+const toBigInt = (neo4jInt) => {
+    return neo4jInt.toString();
 }
- 
+
 const toDate = (yearLow, monthLow, dayLow) => {
     return new Date(yearLow, monthLow - 1, dayLow); // Months are 0-indexed in JS Date
-  }
+}
 
-  const convertNeo4jProps = (obj) => {
+const convertNeo4jProps = (obj) => {
     if (Array.isArray(obj)) {
-      return obj.map(convertNeo4jProps);
+        return obj.map(convertNeo4jProps);
     } else if (obj && typeof obj === 'object') {
-      return Object.fromEntries(
-        Object.entries(obj).map(([key, value]) => {
-          if (neo4j.isInt(value)) {
-            return [key, toBigInt(value.low, value.high).toString()];
-          } else if (value.year && value.month && value.day) {
-            return [key, toDate(value.year.low, value.month.low, value.day.low)];
-          } else {
-            return [key, convertNeo4jProps(value)];
-          }
-        })
-      );
+        return Object.fromEntries(
+            Object.entries(obj).map(([key, value]) => {
+                if (neo4j.isInt(value)) {
+                    return [key, toBigInt(value)];
+                } else if (value.year && value.month && value.day) {
+                    return [key, toDate(value.year.low, value.month.low, value.day.low)];
+                } else {
+                    return [key, convertNeo4jProps(value)];
+                }
+            })
+        );
     } else {
-      return obj;
+        return obj;
     }
-  }
+}
 
 module.exports = {toBigInt, toDate, convertNeo4jProps}
