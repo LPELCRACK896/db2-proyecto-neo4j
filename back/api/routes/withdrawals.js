9d933afdf02ef1abfe82bed6ea9e61193c85d317
Luis Pedro Gonzalez Aldana <60456157+LPELCRACK896@users.noreply.github.com>
2023-05-31 18:25:59 -0600

    POST Create withdrwal

--- back/api/routes/withdrawals.js
+++ back/api/routes/withdrawals.js
@@ -1,12 +1,14 @@
 const express = require('express')
 const {
     getWithdrawals,
-    getWithdrawal
+    getWithdrawal,
+    createWithdrawal
  } = require('../controllers/withdrawals')
-
+ const fieldChecker = require('../middlewares/fieldChecker');
+ 
 const router = express.Router();
 
-router.route('/').get(getWithdrawals)
+router.route('/').get(getWithdrawals).post(fieldChecker(["amount", "motive","location", "account_number", "dpi", "type"]), createWithdrawal)
 router.route('/:id').get(getWithdrawal)
 
 module.exports = router
