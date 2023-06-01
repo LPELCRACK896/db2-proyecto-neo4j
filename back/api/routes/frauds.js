6e1c246e07ea067f9e08dd505bd785e48372f863
Luis Pedro Gonzalez Aldana <60456157+LPELCRACK896@users.noreply.github.com>
2023-05-31 18:51:41 -0600

    update routes

--- back/api/routes/frauds.js
+++ back/api/routes/frauds.js
@@ -8,7 +8,7 @@ const {
 const router = express.Router();
 
 router.route('/deposit').get(getSuspiciosDepositFraud)
-router.route('/discrep').get(getDicrepanceByIngreso)
-router.route('/suswithdrawal').get(getSusRetirment)
+router.route('/ingreso').get(getDicrepanceByIngreso)
+router.route('/withdrawal').get(getSusRetirment)
 
 module.exports = router
